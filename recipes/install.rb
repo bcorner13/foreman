e3da8155d2ffcc93450d096342218e0847757af8
Bradley Corner <bcorner@fanatics.com>
2016-07-17 17:32:32 -0500

    Added platform family for apache2 vs http

--- recipes/install.rb
+++ recipes/install.rb
@@ -25,41 +25,48 @@ end
 
 package pkg
 
- include_recipe 'apache2'
+case node['platform_family']
+when 'debian'
+  include_recipe 'apache2'
+when 'rhel'
+  include_recipe 'http'
+end
+
+
 # TODO: remove when apache2 will works fine
 #  Recipe: foreman::install
 #          * execute[remove-other-vhost] action run
-#            
+#
 #            ================================================================================
 #            Error executing action `run` on resource 'execute[remove-other-vhost]'
 #            ================================================================================
-#            
+#
 #            Mixlib::ShellOut::ShellCommandFailed
 #            ------------------------------------
 #            Expected process to exit with [0], but received '1'
 #            ---- Begin output of a2disconf other-vhosts-access-log && sleep 2 ----
-#            STDOUT: 
+#            STDOUT:
 #            STDERR: ERROR: Conf other-vhosts-access-log does not exist!
 #            ---- End output of a2disconf other-vhosts-access-log && sleep 2 ----
 #            Ran a2disconf other-vhosts-access-log && sleep 2 returned 1
-#            
+#
 #            Cookbook Trace:
 #            ---------------
 #            /tmp/kitchen/cache/cookbooks/compat_resource/files/lib/chef_compat/monkeypatches/chef/runner.rb:41:in `run_action'
-#            
+#
 #            Resource Declaration:
 #            ---------------------
 #            # In /tmp/kitchen/cache/cookbooks/foreman/recipes/install.rb
-#            
+#
 #             30: execute 'remove-other-vhost' do
 #             31:   command 'a2disconf other-vhosts-access-log && sleep 2'
 #             32: end
-#             33: 
-#            
+#             33:
+#
 #            Compiled Resource:
 #            ------------------
 #           # Declared in /tmp/kitchen/cache/cookbooks/foreman/recipes/install.rb:30:in `from_file'
-#           
+#
 #           execute("remove-other-vhost") do
 #             action [:run]
 #             retries 0
@@ -72,7 +79,7 @@ package pkg
 #             cookbook_name "foreman"
 #             recipe_name "install"
 #           end
-#           
+#
 #           Platform:
 #           ---------
 #           x86_64-linux
